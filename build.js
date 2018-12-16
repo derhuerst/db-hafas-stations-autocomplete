cf754977f9f12d9c56e62d4f9b5def27c8c9907d
Jannis R <mail@jannisr.de>
2018-12-16 21:38:24 +0100

    build script, test :white_check_mark:

--- build.js
+++ build.js
@@ -1,3 +1,43 @@
 'use strict'
 
-// todo
+const fs = require('fs')
+const path = require('path')
+const getHafasStations = require('db-hafas-stations')
+const build = require('synchronous-autocomplete/build')
+const tokenize = require('tokenize-db-station-name')
+
+const showError = (err) => {
+	if (!err) return
+	console.error(err.stack)
+	process.exit(1)
+}
+
+const writeJSON = (file, data, cb) => {
+	fs.writeFile(path.join(__dirname, file), JSON.stringify(data), cb)
+}
+
+console.info('Collecting search items.')
+
+const items = []
+getHafasStations()
+.on('data', (station) => {
+	items.push({
+		id: station.id,
+		name: station.name,
+		weight: station.weight || 1
+	})
+})
+.once('end', () => {
+	console.info('Computing a search index.')
+
+	const {tokens, scores, weights, nrOfTokens, originalIds} = build(tokenize, items)
+
+	console.info('Writing the index to disk.')
+
+	writeJSON('tokens.json', tokens, showError)
+	writeJSON('scores.json', scores, showError)
+	writeJSON('weights.json', weights, showError)
+	writeJSON('nr-of-tokens.json', nrOfTokens, showError)
+	writeJSON('original-ids.json', originalIds, showError)
+})
+.once('error', showError)
--- test.js
+++ test.js
@@ -1,5 +1,18 @@
 'use strict'
 
-const todo = require('.')
+const assert = require('assert')
+const readStations = require('db-hafas-stations')
 
-// todo
+const autocomplete = require('.')
+
+const hasResult = (query, id, limit = 5) => {
+	const results = autocomplete(query, limit)
+	assert.ok(results.find(res => res.id === id), `"${query}": missing ${id}`)
+}
+
+const frankfurtHbf = '8000105'
+hasResult('frankfu', frankfurtHbf)
+const hamburgDammtor = '8002548'
+hasResult('dammt', hamburgDammtor)
+const mittweida = '8012369'
+hasResult('mittwe', mittweida)
